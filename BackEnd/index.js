9f554a3d02519279f660b6085b4f2ed08d9ec317
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-28 22:56:02 +0300

    set up the server

---
+++ BackEnd/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config({ path: "./config/.env" });
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server has started on ${port}`);
+});
