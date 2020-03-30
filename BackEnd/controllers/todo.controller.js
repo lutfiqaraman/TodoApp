07114b3a27cf3947ecd84f9f96a2bf0b209ff344
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-30 22:15:43 +0300

    get all todos

--- BackEnd/controllers/todo.controller.js
+++ BackEnd/controllers/todo.controller.js
@@ -5,10 +5,10 @@ exports.createTodo = async (req, res) => {
   const data = req.body;
   
   try {
-	const sql = 
-		`INSERT INTO todo(todo_desc, is_completed) 
-		VALUES 
-		('${data.todo_desc}', ${data.is_completed})`;
+    const sql = 
+		  `INSERT INTO todo(todo_desc, is_completed) 
+		  VALUES 
+		  ('${data.todo_desc}', ${data.is_completed})`;
 		
     db.dbConnection.query(sql);
     res.status(201).send(data);
@@ -18,7 +18,18 @@ exports.createTodo = async (req, res) => {
 };
 
 // Get all to-do tasks
-exports.getAlltodos = () => {};
+exports.getAlltodos = async(req, res) => {
+  try {
+    const sql = "SELECT * FROM todo";
+  
+    db.dbConnection.query(sql, (err, result, fields) => {
+      if (err) throw res.status(400).send(err.message);
+      res.status(200).send({ result, fields });
+    });
+    } catch (error) {
+      if (error) throw error;
+    }
+};
 
 // Get a to-do task
 exports.getTodo = () => {};
--- BackEnd/routes/todo.routes.js
+++ BackEnd/routes/todo.routes.js
@@ -2,4 +2,5 @@ module.exports = app => {
     const toDO = require("../controllers/todo.controller");
 
     app.post("/todos/todo/create", toDO.createTodo);
+    app.get("/todos", toDO.getAlltodos);
 };
