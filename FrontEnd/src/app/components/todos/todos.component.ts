64120da457e7c890914f7a4b329c7122ed695684
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-01 18:33:54 +0300

    get the list of todos in todos component

--- FrontEnd/src/app/components/todos/todos.component.ts
+++ FrontEnd/src/app/components/todos/todos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TodosService } from 'src/app/services/todos.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todos: object;
 
-  constructor() { }
+  constructor(private todosservice: TodosService) { }
 
   ngOnInit(): void {
+    this.todosservice.getTodos().subscribe((data) => {
+      this.todos = data;
+    });
   }
 
 }
